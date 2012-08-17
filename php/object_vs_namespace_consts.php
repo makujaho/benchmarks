1de33d762bbd3acaf64f6f7260bdbd83651234ba
MKzero <info@linux-web-development.de>
2012-08-17 17:55:50 +0200

    Skeleton for Object vs Namespaced constants

---
+++ php/object_vs_namespace_consts.php
@@ -0,0 +1,17 @@
+<?php
+
+$testcount      = 1000;
+$testconstants  = array(10,100,10000);
+
+foreach ($testconstants as $constnumber) {
+
+    // Create testfile with namespace
+
+
+    // Create testfile with object
+
+
+    for($i = 0; $i <= $testcount; ++$i) {
+        
+    }
+}
