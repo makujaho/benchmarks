2e5c21234b49ebfffb604f89f14884f4d906d6ac
MKzero <info@linux-web-development.de>
2012-08-27 12:45:46 +0200

    Changed Object vs Namespace constant test to benchframe

--- php/object_vs_namespace_consts.php
+++ php/object_vs_namespace_consts.php
@@ -3,17 +3,20 @@
 ini_set('display_errors','On');
 error_reporting(E_ALL);
 
-define('E', PHP_EOL);
-// TODO: Set *any* code optimizers like APC or Zend Optimizer to disabled
+require_once 'benchframe.php';
 
-$testcount      = 1000;
+class object_vs_namespace extends benchframe {
+    protected $version = 1;
+}
+
+$timer          = new object_vs_namespace();
+$testcount      = 100000;
 $testconstants  = array(10,100,1000,5000);
 $d              = dirname(__FILE__);
 
 foreach ($testconstants as $constnumber) {
 
     echo 'Generating namespace with ' . $constnumber . ' constants:'.E;
-    $t1 = microtime(true);
 
     $constants = '<?php namespace testspace' . $constnumber . ';';
     for ($i = 0; $i <= $constnumber; ++$i) {
@@ -24,7 +27,6 @@ foreach ($testconstants as $constnumber) {
     file_put_contents($n_fname, $constants);
 
     echo 'Generating object with $constnumber constants...'.E;
-    $t2 = microtime(true);
 
     $object = '<?php class testclass' . $constnumber . ' {';
     for ($i = 0; $i <= $constnumber; ++$i) {
@@ -35,10 +37,8 @@ foreach ($testconstants as $constnumber) {
     file_put_contents($o_fname, $object);
 
     echo 'Generated files. Starting tests.'.E;
-    $t3 = microtime(true);
-
     echo 'Accessing constants'.E;
-    $t4 = microtime(true);
+    $timer->add_timing();
 
     require $n_fname;
     for ($i = 0; $i <= $testcount; ++$i) {
@@ -48,7 +48,7 @@ foreach ($testconstants as $constnumber) {
     }
 
     echo 'Accessing object'.E;
-    $t5 = microtime(true);
+    $timer->add_timing('Namespace - Constants: ' . $constnumber);
     
     require $o_fname;
     for ($i = 0; $i <= $testcount; ++$i) {
@@ -56,16 +56,8 @@ foreach ($testconstants as $constnumber) {
         $c = rand(0, $constnumber);
         $t = constant("testclass$constnumber::FFFFFFFFFFFFFFFFFconstant$c");
     }
-    $t6 = microtime(true);
+    $timer->add_timing('Object    - Constants: ' . $constnumber);
 
     echo 'Done testing. Results:'.E;
-    echo 'Namespace: ' . ($t5 - $t4).E;
-    echo 'Object:    ' . ($t6 - $t5).E;
-    echo 'Times: '.E;
-    echo '       1 - ' . $t1.E;
-    echo '       2 - ' . $t2.E;
-    echo '       3 - ' . $t3.E;
-    echo '       4 - ' . $t4.E;
-    echo '       5 - ' . $t5.E;
-    echo '       6 - ' . $t6.E;
+    echo $timer;
 }
